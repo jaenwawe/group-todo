4a61a5935765c0cd5c904a32cb62066fbe4a6167
Tarric Sookdeo <tarricsookdeo@outlook.com>
2021-10-12 14:35:16 -0400

    Add friend request to admin

--- friends/admin.py
+++ friends/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import FriendRequest, FriendsList
+
+admin.site.register(FriendRequest)
+admin.site.register(FriendsList)
