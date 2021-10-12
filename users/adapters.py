047070135a05ea19c6f76c301ab756db2e338836
Tarric Sookdeo <tarricsookdeo@outlook.com>
2021-10-12 01:13:00 -0400

    Comment out user adapter for now. Will add back when implementing the frontend

--- users/adapters.py
+++ users/adapters.py
@@ -1,6 +1,6 @@
 from allauth.account.adapter import DefaultAccountAdapter
 
 
-class CustomAcountAdapter(DefaultAccountAdapter):
-    def get_email_confirmation_url(self, request, emailconfirmation):
-        return f'http://localhost:3000/confirm-email/{emailconfirmation.key}'
+# class CustomAcountAdapter(DefaultAccountAdapter):
+#     def get_email_confirmation_url(self, request, emailconfirmation):
+#         return f'http://localhost:3000/confirm-email/{emailconfirmation.key}'
