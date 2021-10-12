16441f9cc30902db228107959fd7bf591d4951b3
Tarric Sookdeo <tarricsookdeo@outlook.com>
2021-10-12 14:06:36 -0400

    Add FriendList and FriendRequest models

--- friends/models.py
+++ friends/models.py
@@ -1,3 +1,17 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class FriendsList(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    friends = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, blank=True, related_name='friends')
+
+
+class FriendRequest(models.Model):
+    from_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='from_user')
+    to_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='to_user')
+    accepted = models.BooleanField(default=False)
